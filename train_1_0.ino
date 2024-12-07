c222dcd9a7d4f8923f450c07ad6c3c343c48bc4a
ghola90 <ghola90@gmail.com>
2024-12-06 23:51:13 -0500

    Add files via upload
    Ghola Loop Code Version - 1.0 - 12/04/2024 - Make the motor move

---
+++ train_1_0.ino
@@ -0,0 +1,97 @@
+/*
+
+Ghola Loop Code Version - 1.0 - 12/04/2024 - Make the motor move
+
+Pin use:
+
+A0 - 
+A1 - 
+A2 - 
+A3 - 
+A4 - SDA on LCD
+A5 - SCL on LCD
+
+D0 - 
+D1 - 
+D2 - 
+D3 - 
+D4 - 
+D5 - 
+D6 - 
+D7 - 
+D8 - motor driver pin IN2
+D9 - motor driver pin IN1
+D10- motor driver pin ENT
+D11- 
+D12- 
+D13- 
+
+*/
+
+#include <LiquidCrystal_I2C.h>       // inlude required LCD library
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // use this for 16x2 LCD modules
+
+int Motor_1 = 8;  // assign motor driver pin IN2 to pin 8
+int Motor_2 = 9;  // assign motor driver pin IN1 to pin 9
+int ENA = 10;     // assign motor driver pin ENA to pin 10
+
+
+void setup() {
+
+  lcd.init();       // initialize the LCD
+  lcd.backlight();  // turn on the backlight for the LCD
+
+  pinMode(Motor_1, OUTPUT);  // set the mode for the pin INPUT/OUTPUT
+  pinMode(Motor_2, OUTPUT);  // set the mode for the pin INPUT/OUTPUT
+}
+
+
+void loop() {
+
+  lcd.clear();           // clear LCD screen
+  lcd.setCursor(0, 0);   // set cursor to 1st character on 1st line
+  lcd.print("Train 1");  // print x on the LCD screen
+  lcd.setCursor(0, 1);   // set cursor to 1st character on 2nd line
+  lcd.print("STOP");     // print x on the LCD screen
+
+  digitalWrite(Motor_1, LOW);  // the train will stop
+  digitalWrite(Motor_2, LOW);  // the train will stop
+
+  delay(4000);  // delay for (x)
+
+  lcd.clear();               // clear LCD screen
+  lcd.setCursor(0, 0);       // set cursor to 1st character on 1st line
+  lcd.print("Train 1 100");  // print x on the LCD screen
+  lcd.setCursor(0, 1);       // set cursor to 1st character on 2nd line
+  lcd.print("Speeding Up");  // print x on the LCD screen
+
+  analogWrite(ENA, 100);  // set train speed x on 0-255 range
+  digitalWrite(Motor_1, LOW);
+  digitalWrite(Motor_2, HIGH);  // the train will move
+
+  delay(2000);  // delay for (x)
+
+  lcd.clear();               // clear LCD screen
+  lcd.setCursor(0, 0);       // set cursor to 1st character on 1st line
+  lcd.print("Train 1 130");  // print x on the LCD screen
+  lcd.setCursor(0, 1);       // set cursor to 1st character on 2nd line
+  lcd.print("Speeding Up");  // print x on the LCD screen
+
+  analogWrite(ENA, 130);  // set train speed x on 0-255 range
+  digitalWrite(Motor_1, LOW);
+  digitalWrite(Motor_2, HIGH);  // the train will move
+
+  delay(2000);  // delay for (x)
+
+  lcd.clear();               // clear LCD screen
+  lcd.setCursor(0, 0);       // set cursor to 1st character on 1st line
+  lcd.print("Train 1 160");  // print x on the LCD screen
+  lcd.setCursor(0, 1);       // set cursor to 1st character on 2nd line
+  lcd.print("Full Speed ");  // print x on the LCD screen
+
+  analogWrite(ENA, 160);  // set train speed x on 0-255 range
+  digitalWrite(Motor_1, LOW);
+  digitalWrite(Motor_2, HIGH);  // the train will move
+
+  delay(2000);  // delay for (x)
+}
